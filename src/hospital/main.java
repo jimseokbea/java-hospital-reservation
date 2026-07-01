7a4d3030f5669e30b89a6251f5914cb78e0e5a6d
jinseok Bea <115687246+jimseokbea@users.noreply.github.com>
2026-07-02 00:31:39 +0900

    feat: add menu selection with conditional statements

--- src/hospital/main.java
+++ src/hospital/main.java
@@ -1,9 +1,13 @@
 package hospital;
 
+import java.util.Scanner;
+
+import static java.awt.SystemColor.menu;
+
 public class main {
     public static void main(String[] args) {
 
-        //
+        /*
         String userName = "배진석";
         int userAge = 27;
 
@@ -19,5 +23,30 @@ public class main {
         System.out.println("진료과 : " + medicalDepartment);
         System.out.println("예약일 : " + resrvationDate);
         System.out.println("예약시간 : " + resrvationTime);
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("== 병원 예약콘솔 프로그램 ==");
+        System.out.println("1. 회원등록");
+        System.out.println("2. 병원 목록 보기");
+        System.out.println("3. 예약하기");
+        System.out.println("4. 종료");
+        System.out.print("메뉴를 선택하세요 : ");
+
+        int menu =scanner.nextInt();
+
+        if (menu == 1){
+            System.out.println("회원 등록 기능입니다.");
+        }else if (menu == 2){
+            System.out.println("병원 목록 보기 기능입니다.");
+        }else if (menu == 3){
+            System.out.println("예약하기 기능입니다");
+        }else if (menu == 0){
+            System.out.println("종료");
+        }else {
+            System.out.println("잘못된 메뉴입니다.");
+        }
+
+        scanner.close();
     }
 }
